431fb425dfb857c4853200de559a055603cec34e
Antonio Pisanello <antonio@apisanello.dev>
2024-01-22 10:20:56 +0100

    ajouter deux lignes a test_nodejs

--- test_nodejs.js
+++ test_nodejs.js
@@ -1,2 +1,6 @@
 console.log("NodeJS test successful!");
-console.log(`NodeJS version: ${process.version}`);
+console.log(`NodeJS version: ${process.version}`)
+
+console.log("hello");
+
+console.log("hello_2")
