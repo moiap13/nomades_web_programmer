34d79923a9a8635750056a6526d320a7e7a70ae1
Antonio Pisanello <antonio@apisanello.dev>
2024-04-25 12:41:15 +0200

    Add userlist exercise

---
+++ student_exercises/implementations/DOM/usersList/usersList_eleves/users.js
@@ -0,0 +1,236 @@
+const USERS = [
+  {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        // "lat": "-37.3159",
+        "lat": 46.2044,
+        // "lng": "81.1496"
+        "lng": 6.1432
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  },
+  {
+    "id": 2,
+    "name": "Ervin Howell",
+    "username": "Antonette",
+    "email": "Shanna@melissa.tv",
+    "address": {
+      "street": "10, Rue de la Paix",
+      "suite": "Suite 879",
+      "city": "Paris",
+      "zipcode": "75002",
+      "geo": {
+        "lat": "48.8706",
+        "lng": "2.3326"
+      }
+    },
+    "phone": "010-692-6593 x09125",
+    "website": "anastasia.net",
+    "company": {
+      "name": "Deckow-Crist",
+      "catchPhrase": "Proactive didactic contingency",
+      "bs": "synergize scalable supply-chains"
+    }
+  },
+  {
+    "id": 3,
+    "name": "Clementine Bauch",
+    "username": "Samantha",
+    "email": "Nathan@yesenia.net",
+    "address": {
+      "street": "Piazza di Trevi,",
+      "suite": "Suite 847",
+      "city": "Roma",
+      "zipcode": "00187",
+      "geo": {
+        "lat": "41.9009",
+        "lng": "12.4833"
+      }
+    },
+    "phone": "1-463-123-4447",
+    "website": "ramiro.info",
+    "company": {
+      "name": "Romaguera-Jacobson",
+      "catchPhrase": "Face to face bifurcated interface",
+      "bs": "e-enable strategic applications"
+    }
+  },
+  {
+    "id": 4,
+    "name": "Patricia Lebsack",
+    "username": "Karianne",
+    "email": "Julianne.OConner@kory.org",
+    "address": {
+      "street": "La Rambla",
+      "suite": "Apt. 692",
+      "city": "Barcelona",
+      "zipcode": "08002",
+      "geo": {
+        "lat": "41.3818",
+        "lng": "2.1685"
+      }
+    },
+    "phone": "493-170-9623 x156",
+    "website": "kale.biz",
+    "company": {
+      "name": "Robel-Corkery",
+      "catchPhrase": "Multi-tiered zero tolerance productivity",
+      "bs": "transition cutting-edge web services"
+    }
+  },
+  {
+    "id": 5,
+    "name": "Chelsey Dietrich",
+    "username": "Kamren",
+    "email": "Lucio_Hettinger@annie.ca",
+    "address": {
+      "street": "Brandenburg Gate",
+      "suite": "Suite 351",
+      "city": "Berlin",
+      "zipcode": "10117",
+      "geo": {
+        "lat": "52.5163",
+        "lng": "13.4050"
+      }
+    },
+    "phone": "(254)954-1289",
+    "website": "demarco.info",
+    "company": {
+      "name": "Keebler LLC",
+      "catchPhrase": "User-centric fault-tolerant solution",
+      "bs": "revolutionize end-to-end systems"
+    }
+  },
+  {
+    "id": 6,
+    "name": "Mrs. Dennis Schulist",
+    "username": "Leopoldo_Corkery",
+    "email": "Karley_Dach@jasper.info",
+    "address": {
+      "street": "Dam Square,",
+      "suite": "Apt. 950",
+      "city": "Amsterdam",
+      "zipcode": "1012",
+      "geo": {
+        "lat": "52.3731",
+        "lng": "4.8922"
+      }
+    },
+    "phone": "1-477-935-8478 x6430",
+    "website": "ola.org",
+    "company": {
+      "name": "Considine-Lockman",
+      "catchPhrase": "Synchronised bottom-line interface",
+      "bs": "e-enable innovative applications"
+    }
+  },
+  {
+    "id": 7,
+    "name": "Kurtis Weissnat",
+    "username": "Elwyn.Skiles",
+    "email": "Telly.Hoeger@billy.biz",
+    "address": {
+      "street": "Acropolis",
+      "suite": "Suite 280",
+      "city": "Athens",
+      "zipcode": "10558",
+      "geo": {
+        "lat": "37.9838",
+        "lng": "23.7275"
+      }
+    },
+    "phone": "210.067.6132",
+    "website": "elvis.io",
+    "company": {
+      "name": "Johns Group",
+      "catchPhrase": "Configurable multimedia task-force",
+      "bs": "generate enterprise e-tailers"
+    }
+  },
+  {
+    "id": 8,
+    "name": "Nicholas Runolfsdottir V",
+    "username": "Maxime_Nienow",
+    "email": "Sherwood@rosamond.me",
+    "address": {
+      "street": "Praça do Comércio",
+      "suite": "Suite 729",
+      "city": "Lisbon",
+      "zipcode": "1100-148",
+      "geo": {
+        "lat": "38.7072",
+        "lng": "-9.1393"
+      }
+    },
+    "phone": "586.493.6943 x140",
+    "website": "jacynthe.com",
+    "company": {
+      "name": "Abernathy Group",
+      "catchPhrase": "Implemented secondary concept",
+      "bs": "e-enable extensible e-tailers"
+    }
+  },
+  {
+    "id": 9,
+    "name": "Glenna Reichert",
+    "username": "Delphine",
+    "email": "Chaim_McDermott@dana.io",
+    "address": {
+      "street": "Stephansplatz",
+      "suite": "Suite 449",
+      "city": "Vienna",
+      "zipcode": "1010",
+      "geo": {
+        "lat": "48.2082",
+        "lng": "16.3738"
+      }
+    },
+    "phone": "(775)976-6794 x41206",
+    "website": "conrad.com",
+    "company": {
+      "name": "Yost and Sons",
+      "catchPhrase": "Switchable contextually-based project",
+      "bs": "aggregate real-time technologies"
+    }
+  },
+  {
+    "id": 10,
+    "name": "Clementina DuBuque",
+    "username": "Moriah.Stanton",
+    "email": "Rey.Padberg@karina.biz",
+    "address": {
+      "street": "Trinity College Dublin",
+      "suite": "Suite 198",
+      "city": "Dublin",
+      "zipcode": "2",
+      "geo": {
+        "lat": "53.3438",
+        "lng": "-6.2546"
+      }
+    },
+    "phone": "024-648-3804",
+    "website": "ambrose.net",
+    "company": {
+      "name": "Hoeger LLC",
+      "catchPhrase": "Centralized empowering task-force",
+      "bs": "target end-to-end models"
+    }
+  }
+]
+
+console.log(USERS)
